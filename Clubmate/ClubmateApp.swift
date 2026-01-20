12f9c3093c81c2491206f95beb7c6fc740ebd753
Joe <joejoe98t@gmail.com>
2026-01-20 12:49:10 +0000

    Initial Commit

---
+++ Clubmate/ClubmateApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClubmateApp.swift
+//  Clubmate
+//
+//  Created by Lincy George on 20/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct ClubmateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
